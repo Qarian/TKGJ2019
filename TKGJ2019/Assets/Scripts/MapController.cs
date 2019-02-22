a6c39bbc33ed120f478f50dda44c0bf32278c48e
Qarian <Ferno>
2019-02-23 00:35:04 +0100

    Zones

--- TKGJ2019/Assets/Scripts/MapController.cs
+++ TKGJ2019/Assets/Scripts/MapController.cs
@@ -1,6 +1,25 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class MapController : MonoBehaviour
 {
-    
+	public float startScale;
+	public float smallerScale;
+	public float increaseSize;
+
+	List<Zone> list = new List<Zone>();
+
+    SpriteMask mask;
+
+	void Start()
+	{
+		mask = GetComponent<SpriteMask>();
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			list.Add(transform.GetChild(i).GetComponent<Zone>());
+			list[i].SetScale(startScale);
+		}
+		Zone.smallerScale = smallerScale;
+		Zone.increaseSize = increaseSize;
+	}
 }
---
+++ TKGJ2019/Assets/Scripts/Zone.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Zone : MonoBehaviour
+{
+	public static float smallerScale;
+	public static float increaseSize;
+	// Easy to access object scale
+    float scale;
+
+	// Change SmallerScale
+	public void ChangeScale()
+	{
+		scale *= smallerScale;
+		transform.localScale = new Vector3(scale, scale);
+	}
+
+	public void IncreaseScale()
+	{
+		scale += increaseSize;
+		transform.localScale = new Vector3(scale, scale);
+	}
+
+	// Set scale (to use on Start)
+	public void SetScale(float targetScale)
+	{
+		scale = targetScale;
+		transform.localScale = new Vector3(scale, scale);
+	}
+}
